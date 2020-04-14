20b31ba4a79b1b481b53ce0acdd041907f6c5310
Addy Osmani <addyosmani@gmail.com>
2020-04-14 14:37:32 -0700

    [core] Add initial options support

---
+++ extension/src/options/options.js
@@ -0,0 +1,28 @@
+// Saves options to chrome.storage
+function save_options() {
+    const enableOverlay = document.getElementById('overlay').checked;
+    chrome.storage.sync.set({
+        enableOverlay
+    }, () => {
+        // Update status to let user know options were saved.
+        const status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(() => {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value color = 'red' and enableOverlay = true.
+    chrome.storage.sync.get({
+        enableOverlay: true
+    }, ({enableOverlay}) => {
+        document.getElementById('overlay').checked = enableOverlay;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
