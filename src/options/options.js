3f7b0258ec2534d11edd0019138a8504a2ee5fb5
Rick Viscomi <rviscomi@users.noreply.github.com>
2023-04-26 10:30:57 -0400

    updated web vitals logging (#119)

--- src/options/options.js
+++ src/options/options.js
@@ -1,7 +1,6 @@
 const optionsOverlayNode = document.getElementById('overlay');
 const optionsConsoleLoggingNode = document.getElementById('consoleLogging');
 const optionsUserTimingNode = document.getElementById('userTiming');
-const optionsConsoleTablesNode = document.getElementById('consoleTables');
 const optionsPreferPhoneFieldNode = document.getElementById('preferPhoneField');
 const optionsSaveBtn = document.getElementById('save');
 const optionsStatus = document.getElementById('status');
@@ -14,7 +13,6 @@ function saveOptions() {
     enableOverlay: optionsOverlayNode.checked,
     debug: optionsConsoleLoggingNode.checked,
     userTiming: optionsUserTimingNode.checked,
-    consoleTables: optionsConsoleTablesNode.checked,
     preferPhoneField: optionsPreferPhoneFieldNode.checked,
   }, () => {
     // Update status to let user know options were saved.
@@ -34,13 +32,11 @@ function restoreOptions() {
     enableOverlay: false,
     debug: false,
     userTiming: false,
-    consoleTables: false,
     preferPhoneField: false,
-  }, ({enableOverlay, debug, userTiming, consoleTables, preferPhoneField}) => {
+  }, ({enableOverlay, debug, userTiming, preferPhoneField}) => {
     optionsOverlayNode.checked = enableOverlay;
     optionsConsoleLoggingNode.checked = debug;
     optionsUserTimingNode.checked = userTiming;
-    optionsConsoleTablesNode.checked = consoleTables;
     optionsPreferPhoneFieldNode.checked = preferPhoneField;
   });
 }
